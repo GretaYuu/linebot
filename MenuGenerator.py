a1b5089bf84a5c14e33c5bdcbc1b8a20415bfbff
GretaYuu <132889038+GretaYuu@users.noreply.github.com>
2023-06-07 00:08:04 +0800

    Add files via upload

--- MenuGenerator.py
+++ MenuGenerator.py
@@ -63,39 +63,24 @@ class MenuGenerator:
           }
         }, {
           "type": "button",
-          "style": "link",
-          "height": "sm",
           "action": {
-            "type": "uri",
+            "type": "postback",
             "label": "中部地區",
-            "uri": "https://linecorp.com"
+            "data": "areaC"
           }
         }, {
           "type": "button",
-          "style": "link",
-          "height": "sm",
           "action": {
-            "type": "uri",
+            "type": "postback",
             "label": "南部地區",
-            "uri": "https://linecorp.com"
+            "data": "areaS"
           }
         }, {
           "type": "button",
-          "style": "link",
-          "height": "sm",
           "action": {
-            "type": "uri",
-            "label": "花東地區",
-            "uri": "https://linecorp.com"
-          }
-        }, {
-          "type": "button",
-          "style": "link",
-          "height": "sm",
-          "action": {
-            "type": "uri",
-            "label": "離島地區",
-            "uri": "https://linecorp.com"
+            "type": "postback",
+            "label": "花東及離島地區",
+            "data": "areaE"
           }
         }]
       }
@@ -104,7 +89,117 @@ class MenuGenerator:
 
   @staticmethod
   def generate_areaN_menu():
-    areaN_menu = {
+    areaN = {
+      "type": "bubble",
+      "body": {
+        "type":
+        "box",
+        "layout":
+        "vertical",
+        "contents": [{
+          "type": "text",
+          "weight": "bold",
+          "size": "xl",
+          "text": "選擇北部縣市"
+        }, {
+          "type":
+          "box",
+          "layout":
+          "vertical",
+          "margin":
+          "lg",
+          "spacing":
+          "sm",
+          "contents": [{
+            "type":
+            "box",
+            "layout":
+            "baseline",
+            "spacing":
+            "sm",
+            "contents": [{
+              "type": "text",
+              "text": "Place",
+              "color": "#aaaaaa",
+              "size": "sm",
+              "flex": 1
+            }, {
+              "type": "text",
+              "text": "FLEX MESSAGE SIMULATOR   製作",
+              "wrap": True,
+              "color": "#666666",
+              "size": "sm",
+              "flex": 5
+            }]
+          }]
+        }]
+      },
+      "footer": {
+        "type":
+        "box",
+        "layout":
+        "vertical",
+        "spacing":
+        "sm",
+        "contents": [{
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "台北市",
+            "data": "food_type"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "新北市",
+            "data": "food_type"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "基隆市",
+            "data": "food_type"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "新竹市",
+            "data": "food_type"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "桃園市",
+            "data": "food_type"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "新竹縣",
+            "data": "food_type"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "宜蘭縣",
+            "data": "food_type"
+          }
+        }],
+        "flex":
+        0
+      }
+    }
+    return areaN
+
+  @staticmethod
+  def generate_areaC_menu():
+    areaC = {
       "type": "bubble",
       "hero": {
         "type": "image",
@@ -125,7 +220,7 @@ class MenuGenerator:
         "vertical",
         "contents": [{
           "type": "text",
-          "text": "選擇北部地區縣市",
+          "text": "選擇中部地區縣市",
           "weight": "bold",
           "size": "xl"
         }]
@@ -139,11 +234,287 @@ class MenuGenerator:
         "sm",
         "contents": [{
           "type": "button",
-          "style": "link",
-          "height": "sm",
+          "action": {
+            "type": "postback",
+            "label": "苗栗縣",
+            "data": "food_type"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "臺中市",
+            "data": "food_type"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "彰化縣",
+            "data": "food_type"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "南投縣",
+            "data": "food_type"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "雲林縣",
+            "data": "food_type"
+          }
+        }],
+        "flex":
+        0
+      }
+    }
+    return areaC
+
+  @staticmethod
+  def generate_areaS_menu():
+    areaS = {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "url":
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.lobby0800.com.tw%2Fcemetery-introduction.html&psig=AOvVaw3yLt0SNDpHpdhvLjGAgW9Z&ust=1685979649413000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCMCY0-L5qf8CFQAAAAAdAAAAABAK",
+        "size": "full",
+        "aspectRatio": "20:13",
+        "aspectMode": "cover",
+        "action": {
+          "type": "uri",
+          "uri": "http://linecorp.com/"
+        }
+      },
+      "body": {
+        "type":
+        "box",
+        "layout":
+        "vertical",
+        "contents": [{
+          "type": "text",
+          "text": "選擇南部地區縣市",
+          "weight": "bold",
+          "size": "xl"
+        }]
+      },
+      "footer": {
+        "type":
+        "box",
+        "layout":
+        "vertical",
+        "spacing":
+        "sm",
+        "contents": [{
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "高雄市",
+            "data": "food_type"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "臺南市",
+            "data": "food_type"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "嘉義市",
+            "data": "food_type"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "嘉義縣",
+            "data": "food_type"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "屏東縣",
+            "data": "food_type"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "澎湖縣",
+            "data": "food_type"
+          }
+        }],
+        "flex":
+        0
+      }
+    }
+    return areaS
+
+  @staticmethod
+  def generate_areaE_menu():
+    areaE = {
+      "type": "bubble",
+      "hero": {
+        "type": "image",
+        "url":
+        "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.lobby0800.com.tw%2Fcemetery-introduction.html&psig=AOvVaw3yLt0SNDpHpdhvLjGAgW9Z&ust=1685979649413000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCMCY0-L5qf8CFQAAAAAdAAAAABAK",
+        "size": "full",
+        "aspectRatio": "20:13",
+        "aspectMode": "cover",
+        "action": {
+          "type": "uri",
+          "uri": "http://linecorp.com/"
+        }
+      },
+      "body": {
+        "type":
+        "box",
+        "layout":
+        "vertical",
+        "contents": [{
+          "type": "text",
+          "text": "選擇東部及離島地區縣市",
+          "weight": "bold",
+          "size": "xl"
+        }]
+      },
+      "footer": {
+        "type":
+        "box",
+        "layout":
+        "vertical",
+        "spacing":
+        "sm",
+        "contents": [{
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "花蓮縣",
+            "data": "food_type"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "臺東縣",
+            "data": "food_type"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "金門縣",
+            "data": "food_type"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "postback",
+            "label": "連江縣",
+            "data": "food_type"
+          }
+        }],
+        "flex":
+        0
+      }
+    }
+    return areaE
+
+  @staticmethod
+  def food_type_menu():
+    food_type = {
+      "type": "bubble",
+      "body": {
+        "type":
+        "box",
+        "layout":
+        "vertical",
+        "contents": [{
+          "type": "text",
+          "weight": "bold",
+          "size": "xl",
+          "text": "美食類別選單"
+        }, {
+          "type":
+          "box",
+          "layout":
+          "vertical",
+          "margin":
+          "lg",
+          "spacing":
+          "sm",
+          "contents": [{
+            "type":
+            "box",
+            "layout":
+            "baseline",
+            "spacing":
+            "sm",
+            "contents": [{
+              "type": "text",
+              "text": "Place",
+              "color": "#aaaaaa",
+              "size": "sm",
+              "flex": 1
+            }, {
+              "type": "text",
+              "text": "FLEX MESSAGE SIMULATOR   製作",
+              "wrap": True,
+              "color": "#666666",
+              "size": "sm",
+              "flex": 5
+            }]
+          }]
+        }]
+      },
+      "footer": {
+        "type":
+        "box",
+        "layout":
+        "vertical",
+        "spacing":
+        "sm",
+        "contents": [{
+          "type": "button",
           "action": {
             "type": "uri",
-            "label": "臺北市",
+            "label": "鍋類",
+            "uri": "https://linecorp.com"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "uri",
+            "label": "日式",
+            "uri": "https://linecorp.com"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "uri",
+            "label": "燒肉",
+            "uri": "https://linecorp.com"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "uri",
+            "label": "早午餐",
+            "uri": "https://linecorp.com"
+          }
+        }, {
+          "type": "button",
+          "action": {
+            "type": "uri",
+            "label": "甜點類",
             "uri": "https://linecorp.com"
           }
         }, {
@@ -152,7 +523,7 @@ class MenuGenerator:
           "height": "sm",
           "action": {
             "type": "uri",
-            "label": "新北市",
+            "label": "韓式",
             "uri": "https://linecorp.com"
           }
         }, {
@@ -161,43 +532,7 @@ class MenuGenerator:
           "height": "sm",
           "action": {
             "type": "uri",
-            "label": "基隆市",
-            "uri": "https://linecorp.com"
-          }
-        }, {
-          "type": "button",
-          "style": "link",
-          "height": "sm",
-          "action": {
-            "type": "uri",
-            "label": "新竹市",
-            "uri": "https://linecorp.com"
-          }
-        }, {
-          "type": "button",
-          "style": "link",
-          "height": "sm",
-          "action": {
-            "type": "uri",
-            "label": "桃園市",
-            "uri": "https://linecorp.com"
-          }
-        }, {
-          "type": "button",
-          "style": "link",
-          "height": "sm",
-          "action": {
-            "type": "uri",
-            "label": "新竹縣",
-            "uri": "https://linecorp.com"
-          }
-        }, {
-          "type": "button",
-          "style": "link",
-          "height": "sm",
-          "action": {
-            "type": "uri",
-            "label": "宜蘭縣",
+            "label": "餐酒館/酒吧",
             "uri": "https://linecorp.com"
           }
         }],
@@ -205,4 +540,4 @@ class MenuGenerator:
         0
       }
     }
-    return areaN_menu
+    return food_type
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from linebot.models import (
   FlexSendMessage,
 )
 import os
-
+import mongo
 from MenuGenerator import MenuGenerator
 
 app = Flask(__name__)
@@ -24,15 +24,38 @@ handler = WebhookHandler(secret)
 @app.route('/callback', methods=['POST'])
 def callback():
   # 取得 Line Messaging API 事件
-  events = request.get_json(force=True)['events']
+  #events = request.get_json(force=True)['events']
+  events = request.get_json()['events']
+
+  city_logic_mapping = {
+    '台北市': location_taipei_city,
+    #'高雄市': location_kaohsiung_city,
+    #'台中市': location_taichung_city,
+    # 其他城市與對應的處理邏輯
+  }
+
+  def handle_query():
+    event = request.get_json(force=True)['events'][0]  # 獲取事件物件
+    location = event['postback']['data']
+    print(location)
+    result = query_location(location)
+    return result
+    # 將結果組成回覆訊息回傳給使用者
+    # ...
+
+  def query_location(location):
+    # 執行資料庫查詢
+    results = collection.find({'Region': '臺北市'}).limit(5)
+    print(results)
+    return results
 
   # 迭代處理每個事件
   for event in events:
     if event['type'] == 'message' and event['message']['type'] == 'text':
       user_message = event['message']['text']
-      print('收到使用者輸入: ', user_message)
+      #print('收到使用者輸入: ', user_message)
       if user_message == '開始使用':
-        print("判斷式沒錯")
+        # print("判斷式沒錯")
         reply_token = event['replyToken']
         btntp = {
           "type": "bubble",
@@ -141,115 +164,235 @@ def callback():
           line_bot_api.reply_message(
             event['replyToken'],
             FlexSendMessage(alt_text='開始選單', contents=btntp))
-
+    #景點2
     elif event['type'] == 'postback':
       data = event['postback']['data']
       if data == 'menu2':
         reply_token = event['replyToken']
         # 建立景點地區選單的 JSON
         another_menu = {
-          "type": "bubble",
-          "body": {
-            "type":
-            "box",
-            "layout":
-            "vertical",
-            "contents": [{
-              "type": "text",
-              "weight": "bold",
-              "size": "xl",
-              "text": "縣市選擇"
-            }, {
+          "type":
+          "carousel",
+          "contents": [{
+            "type": "bubble",
+            "header": {
               "type":
               "box",
               "layout":
               "vertical",
-              "margin":
-              "lg",
-              "spacing":
-              "sm",
               "contents": [{
-                "type":
-                "box",
-                "layout":
-                "baseline",
-                "spacing":
-                "sm",
-                "contents": [{
-                  "type": "text",
-                  "text": "Place",
-                  "color": "#aaaaaa",
-                  "size": "sm",
-                  "flex": 1
-                }, {
-                  "type": "text",
-                  "text": "FLEX MESSAGE SIMULATOR   製作",
-                  "wrap": True,
-                  "color": "#666666",
-                  "size": "sm",
-                  "flex": 5
-                }]
+                "type": "image",
+                "url":
+                "https://lh3.googleusercontent.com/Ul1S7hbqOqUG-Z-5CtmuksZJjJ8_v-cbDvAIWkgMhZbVFpJSnrV4NIbd3Y-yvZjhyIiBVLAlpRyk9GywBFFfV5AeqQLEpMpTYaPtV4ibTwdYiJ8BKt_PGNOakYZxIZbJh-YEJHM0CM5_TZCyts-s8Mz_7pFABMeQiQ-8FLnYTqIoaYK8-0mpqBQmEP3E70c1W7DznvJxPGcI1KNr_x-cLex42-S1mWF0h3fp3nRkQL06vF91zAvT9mYPD37K-KvRA55BvFeLg9R8jsK1fmCfk8LIE7onJhzL3MP-eMydfArwa27qvBQImUKgmuTH63s-sSqURKEbvV3GygYDHkyVaGlzX3aqYL-NyJB09EWRhP8PYTuVE_6wVTZ66tPIjJfSHw5dDNNjUCMjlRRnsXtZw7CddKgH0u-1fSI8s8AEvtpVWYAqBBUZR-gy767aWm55HxFztkFcqkgWxHMsPinorejFkPhCg68DqksumUh5lBUc5C5PAXr6HfXigzOGnBlemds8mDXdzKeceiTW0Q1MbhcNEacpvHacub93Ka2z9IZgmmqM5Ih6t9HDxUt6tiONln2ogM1Jnsl4cPieWXGssHxuiZa6bL_ErSgvX_axSyF9K9Oy0SiLvmqo_dey9ilPqSKAioo7faHTBYMLaccZ-yt4KEbJzKZhUty_5yMGX9V4Y3tKZUuWAA2xp0o1Ajxspp4nbjanMtU0ZW_WVQPJvRsUahjjt-FUAX9jKqjKpBdzdmkWLoTUDVWtN0g7hz5IaEl5PyM5q3bY6lWUl4rBvVCtb2szWqCBSgdpsJgbuaP64p1f-Fz6e6-B1sevFzYEfPACnDDTWLkTyEsi4xWf-Utb-U6HXhTPtCix1NKem7JiKfbGCHOgc7uuPKvPiASJRatwa1WG3qOwnwQlXlkMn4YtmKd4FH78SUgh4FBgdzrwR2HsOrjMtw6iKwUm0hECjD6XmYjliJWAmKjzNsEoOBIZ2fO8-l93UpQO5fNe3x60jLROOMaPzbv3In6MyfQrz4KiPkWdKsdiHvxDg0nmLGwIEjA=w650-h748-s-no?authuser=0",
+                "size": "5xl"
               }]
-            }]
-          },
-          "footer": {
-            "type":
-            "box",
-            "layout":
-            "vertical",
-            "spacing":
-            "sm",
-            "contents": [{
-              "type": "button",
-              "style": "link",
-              "height": "sm",
-              "action": {
-                "type": "uri",
-                "label": "台北市",
-                "uri": "https://linecorp.com"
-              }
-            }, {
-              "type": "button",
-              "style": "link",
-              "height": "sm",
-              "action": {
-                "type": "uri",
-                "label": "高雄市",
-                "uri": "https://linecorp.com"
-              }
-            }, {
-              "type": "button",
-              "action": {
-                "type": "uri",
-                "label": "action",
-                "uri": "http://linecorp.com/"
-              }
-            }],
-            "flex":
-            0
-          }
+            },
+            "hero": {
+              "type":
+              "box",
+              "layout":
+              "vertical",
+              "contents": [{
+                "type": "text",
+                "text": "向右滑查看更多縣市",
+                "align": "center"
+              }],
+              "position":
+              "relative"
+            },
+            "body": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": []
+            }
+          }, {
+            "type": "bubble",
+            "header": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [{
+                "type": "text",
+                "text": "北部",
+                "size": "xl"
+              }]
+            },
+            "hero": {
+              "type":
+              "box",
+              "layout":
+              "vertical",
+              "contents": [{
+                "type": "button",
+                "action": {
+                  "type": "postback",
+                  "label": "台北市",
+                  "data": "hello"
+                }
+              }, {
+                "type": "button",
+                "action": {
+                  "type": "postback",
+                  "label": "新北市",
+                  "data": "hello"
+                }
+              }, {
+                "type": "button",
+                "action": {
+                  "type": "postback",
+                  "label": "基隆市",
+                  "data": "hello"
+                }
+              }, {
+                "type": "button",
+                "action": {
+                  "type": "postback",
+                  "label": "桃園縣",
+                  "data": "hello"
+                }
+              }]
+            },
+            "body": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": []
+            }
+          }, {
+            "type": "bubble",
+            "header": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [{
+                "type": "text",
+                "text": "中部",
+                "size": "xl"
+              }]
+            },
+            "hero": {
+              "type":
+              "box",
+              "layout":
+              "vertical",
+              "contents": [{
+                "type": "button",
+                "action": {
+                  "type": "postback",
+                  "label": "台中市",
+                  "data": "hello"
+                }
+              }, {
+                "type": "button",
+                "action": {
+                  "type": "postback",
+                  "label": "南投縣",
+                  "data": "hello"
+                }
+              }, {
+                "type": "button",
+                "action": {
+                  "type": "postback",
+                  "label": "彰化縣",
+                  "data": "hello"
+                }
+              }]
+            },
+            "body": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": []
+            }
+          }]
         }
 
+        # 處理各個城市的邏輯
         line_bot_api.reply_message(
           reply_token, FlexSendMessage(alt_text='景點地區選單',
                                        contents=another_menu))
+      
       elif data == 'menu3':
         reply_token = event['replyToken']
         generator = MenuGenerator()
         another_menu3 = generator.generate_another_menu3()
+
+        # 使用美食選單 JSON
+        line_bot_api.reply_message(
+            reply_token,
+            FlexSendMessage(alt_text='美食地區選單', contents=another_menu3))
+        
+      elif data == 'areaN':
+        reply_token = event['replyToken']
+        generator = MenuGenerator()
         areaN_menu = generator.generate_areaN_menu()
-
-        # 使用選單 JSON
         line_bot_api.reply_message(
-          reply_token,
-          FlexSendMessage(alt_text='美食地區選單', contents=another_menu3))
+            reply_token,
+            FlexSendMessage(alt_text='北部地區選單', contents=areaN_menu))
+      elif data == 'food_type':
+        reply_token = event['replyToken']
+        generator = MenuGenerator()
+        food_type = generator.food_type_menu()
         line_bot_api.reply_message(
-          reply_token,
-          FlexSendMessage(alt_text='北部地區選單',
-                          contents=areaN_menu))  #0606目前北部地區按鈕可按但無該地區縣市返回
-  return 'OK'
+        reply_token,
+        FlexSendMessage(alt_text='食物類型選單', contents=food_type))
+      
+          
+      elif data == 'areaC':
+        reply_token = event['replyToken']
+        generator = MenuGenerator()
+        areaC_menu = generator.generate_areaC_menu()
+        line_bot_api.reply_message(
+            reply_token,
+            FlexSendMessage(alt_text='中部地區選單', contents=areaC_menu))
+      elif data == 'food_type':
+        reply_token = event['replyToken']
+        generator = MenuGenerator()
+        food_type = generator.food_type_menu()
+        line_bot_api.reply_message(
+        reply_token,
+        FlexSendMessage(alt_text='食物類型選單', contents=food_type))
+        
+      elif data == 'areaS':
+        reply_token = event['replyToken']
+        generator = MenuGenerator()
+        areaS_menu = generator.generate_areaS_menu()
+        line_bot_api.reply_message(
+            reply_token,
+            FlexSendMessage(alt_text='南部地區選單', contents=areaS_menu))
+      elif data == 'food_type':
+        reply_token = event['replyToken']
+        generator = MenuGenerator()
+        food_type = generator.food_type_menu()
+        line_bot_api.reply_message(
+        reply_token,
+        FlexSendMessage(alt_text='食物類型選單', contents=food_type))
+        
+      elif data == 'areaE':
+        reply_token = event['replyToken']
+        generator = MenuGenerator()
+        areaE_menu = generator.generate_areaS_menu()
+        line_bot_api.reply_message(
+            reply_token,
+            FlexSendMessage(alt_text='花東及離島地區選單', contents=areaE_menu))
+      elif data == 'food_type':
+        reply_token = event['replyToken']
+        generator = MenuGenerator()
+        food_type = generator.food_type_menu()
+        line_bot_api.reply_message(
+        reply_token,
+        FlexSendMessage(alt_text='食物類型選單', contents=food_type))
+        
+    else:
+        print("又失敗")
+
+    return 'OK'
 
 
-# 使用 Flask 框架的內建伺服器來啟動應用程式
+
+def location_taipei_city():
+  print("測試")
+
+  # 使用 Flask 框架的內建伺服器來啟動應用程式
+
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080)
+  client, db = mongo.connect_to_mongodb()
+  collection = db["attractions"]
